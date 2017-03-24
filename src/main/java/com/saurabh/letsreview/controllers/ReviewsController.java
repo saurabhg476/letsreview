4fc54b570c95510dfed8fb0fad3a45c4a7a3898f
saurabh gupta <saurabh.gupta@novopay.in>
2017-03-24 23:21:58 +0530

    implement get reviews by topic api

--- src/main/java/com/saurabh/letsreview/controllers/ReviewsController.java
+++ src/main/java/com/saurabh/letsreview/controllers/ReviewsController.java
@@ -1,8 +1,12 @@
 package com.saurabh.letsreview.controllers;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,16 +14,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.saurabh.letsreview.datamodel.entity.Review;
+import com.saurabh.letsreview.datamodel.repository.ReviewDAOService;
+
 @Controller
 @ResponseBody
 @RequestMapping(value = "/reviews")
 public class ReviewsController {
 
+	@Autowired
+	private ReviewDAOService reviewDAO;
+
 	@RequestMapping(value = "/{topicName}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String process(HttpServletRequest httpRequest, @PathVariable("topicName") String topicName)
 			throws InterruptedException {
-		JSONObject object = new JSONObject();
-		object.put("topicName", topicName);
-		return object.toString();
+
+		JSONObject response = new JSONObject();
+		JSONArray responseReviewsList = new JSONArray();
+
+		List<Review> reviews = reviewDAO.findReviewsByTopicName(topicName);
+
+		for (Review review : reviews) {
+			JSONObject responseReviewObject = new JSONObject();
+
+			JSONObject responseUserObject = new JSONObject();
+			responseUserObject.put("id", review.getUser().getId());
+			responseUserObject.put("name", review.getUser().getName());
+
+			responseReviewObject.put("user", responseUserObject);
+			responseReviewObject.put("created_on", review.getCreatedOn());
+			responseReviewObject.put("rating", review.getRating());
+			responseReviewObject.put("body", review.getBody());
+
+			responseReviewsList.put(responseReviewObject);
+		}
+
+		response.put("list", responseReviewsList);
+		response.put("topic", topicName);
+
+		return response.toString();
 	}
 }
--- src/main/java/com/saurabh/letsreview/datamodel/repository/ReviewDAOService.java
+++ src/main/java/com/saurabh/letsreview/datamodel/repository/ReviewDAOService.java
@@ -2,10 +2,14 @@ package com.saurabh.letsreview.datamodel.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.saurabh.letsreview.datamodel.entity.Review;
 
 public interface ReviewDAOService extends CrudRepository<Review, Long> {
 	List<Review> findAll();
+
+	@Query(nativeQuery = true, value = "select * from review where review.topic_id = (select id from topic where topic.name = ?1)")
+	public List<Review> findReviewsByTopicName(String topicName);
 }
