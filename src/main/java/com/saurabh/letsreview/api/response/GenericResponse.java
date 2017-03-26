8d152e0441fca0dbe69400585197889eb8144370
saurabh gupta <saurabh.gupta@novopay.in>
2017-03-26 17:40:12 +0530

    add generic response class

---
+++ src/main/java/com/saurabh/letsreview/api/response/GenericResponse.java
@@ -0,0 +1,41 @@
+package com.saurabh.letsreview.api.response;
+
+public class GenericResponse {
+	private String status;
+	private String code;
+	private String message;
+
+	public GenericResponse() {
+
+	}
+
+	public GenericResponse(String status, String code, String message) {
+		this.status = status;
+		this.code = code;
+		this.message = message;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
