92a0fc5da4b01cd5e37920bd2037491b39b37f77
saurabh gupta <saurabh.gupta@novopay.in>
2017-03-27 22:14:18 +0530

    getting username as input in api instead of user_id

--- src/main/java/com/saurabh/letsreview/api/request/PostReviewsRequest.java
+++ src/main/java/com/saurabh/letsreview/api/request/PostReviewsRequest.java
@@ -3,7 +3,7 @@ package com.saurabh.letsreview.api.request;
 public class PostReviewsRequest {
 	private int rating;
 	private String body;
-	private Long user_id;
+	private String username;
 	private String topic_name;
 
 	public int getRating() {
@@ -22,12 +22,12 @@ public class PostReviewsRequest {
 		this.body = body;
 	}
 
-	public Long getUser_id() {
-		return user_id;
+	public String getUsername() {
+		return username;
 	}
 
-	public void setUser_id(Long user_id) {
-		this.user_id = user_id;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	public String getTopic_name() {
--- src/main/java/com/saurabh/letsreview/api/response/ResponseUserObject.java
+++ src/main/java/com/saurabh/letsreview/api/response/ResponseUserObject.java
@@ -1,15 +1,15 @@
 package com.saurabh.letsreview.api.response;
 
 public class ResponseUserObject {
-	private Long id;
+	private String username;
 	private String name;
 
-	public Long getId() {
-		return id;
+	public String getUsername() {
+		return username;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	public String getName() {
