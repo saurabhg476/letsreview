98f3a74d2c3c7987f07d616b58fcd60ed1bb2b05
saurabh gupta <saurabh.gupta@novopay.in>
2017-03-29 00:09:17 +0530

    add method in authenticationDAOService

--- src/main/java/com/project/letsreview/datamodel/repository/AuthenticationDAOService.java
+++ src/main/java/com/project/letsreview/datamodel/repository/AuthenticationDAOService.java
@@ -1,9 +1,12 @@
 package com.project.letsreview.datamodel.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.project.letsreview.datamodel.entity.Authentication;
 
 public interface AuthenticationDAOService extends CrudRepository<Authentication, Long> {
 
+	@Query(nativeQuery = true, value = "select * from authentication where username = ?1")
+	public Authentication findOneByUsername(String username);
 }
