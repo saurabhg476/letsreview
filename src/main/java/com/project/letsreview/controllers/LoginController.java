163e233bff6cb4366f71a1e7c74ebb2d21fb29a2
saurabh gupta <saurabh.gupta@novopay.in>
2017-04-22 15:30:00 +0530

    if user already logged in returning same sessionToken

--- src/main/java/com/project/letsreview/controllers/LoginController.java
+++ src/main/java/com/project/letsreview/controllers/LoginController.java
@@ -49,36 +49,50 @@ public class LoginController {
 			throws CustomGenericException {
 		String username = postLoginRequest.getUsername();
 		String password = postLoginRequest.getPassword();
-		
-		if (!isUserExists(username)) {
-			throw new CustomGenericException("2004");
-		}
 
-		if (isUsernamePasswordMatches(username, password)) {
-			String sessionToken = sessionTokenGenerator.nextSessionToken();
+		checkIfCredentialsAreValid(username, password);
 
-			UserSession session = new UserSession();
-			session.setSessionToken(sessionToken);
-			session.setUsername(username);
+		String sessionToken;
+		if (isAlreadyLoggedIn(username))
+			sessionToken = userSessionDAO.findOneByUsername(username).getSessionToken();
+		else
+			sessionToken = createNewSessionToken(username);
 
-			userSessionDAO.save(session);
+		PostLoginResponse response = createResponse(username, sessionToken);
+		return new ResponseEntity<String>(gson.toJson(response), HttpStatus.OK);
 
-			PostLoginResponse response = new PostLoginResponse();
-
-			response.setCode("00");
-			response.setMessage("User has been logged in successfully");
-			response.setStatus("SUCCESS");
-			response.setUsername(username);
-			response.setSessionToken(sessionToken);
-
-			return new ResponseEntity<String>(gson.toJson(response), HttpStatus.OK);
-
-		} else {
-			throw new CustomGenericException("2003");
-		}
 	}
 
-	private boolean isUserExists(String username){
+	private void checkIfCredentialsAreValid(String username, String password) throws CustomGenericException {
+		if (!isUserExists(username))
+			throw new CustomGenericException("2004");
+		if (!isUsernamePasswordMatches(username, password))
+			throw new CustomGenericException("2003");
+	}
+
+	private PostLoginResponse createResponse(String username, String sessionToken) {
+		PostLoginResponse response = new PostLoginResponse();
+
+		response.setCode("00");
+		response.setMessage("User has been logged in successfully");
+		response.setStatus("SUCCESS");
+		response.setUsername(username);
+		response.setSessionToken(sessionToken);
+		return response;
+	}
+
+	private String createNewSessionToken(String username) {
+		String sessionToken;
+		sessionToken = sessionTokenGenerator.nextSessionToken();
+		UserSession session = new UserSession();
+		session.setSessionToken(sessionToken);
+		session.setUsername(username);
+
+		userSessionDAO.save(session);
+		return sessionToken;
+	}
+
+	private boolean isUserExists(String username) {
 		User user = userDAO.findUserByUsername(username);
 		return user == null ? false : true;
 	}
@@ -88,5 +102,10 @@ public class LoginController {
 		String hashedPassword = new String(Hex.encodeHex(DigestUtils.sha256(password)));
 		String dbRetrivedHashedPassword = authentication.getPassword();
 		return hashedPassword.equals(dbRetrivedHashedPassword) ? true : false;
+	}
+
+	private boolean isAlreadyLoggedIn(String username) {
+		UserSession userSession = userSessionDAO.findOneByUsername(username);
+		return userSession != null;
 	}
 }
